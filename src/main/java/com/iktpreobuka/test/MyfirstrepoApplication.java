ec42587e19d69d41f0867dc06cb63aa4ef3574f7
Milan Celikovic <milancelik@gmail.com>
2024-03-14 20:13:06 +0100

    My first commit from STS

---
+++ src/main/java/com/iktpreobuka/test/MyfirstrepoApplication.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstrepoApplication.class, args);
+	}
+
+}
